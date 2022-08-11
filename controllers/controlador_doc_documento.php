87c5b2d70ca11c5e2c6f7b693515b9376f793fb1
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-11 15:31:45 -0500

    v.0.3.0 Se integra funcion select_documento_id

--- controllers/controlador_doc_documento.php
+++ controllers/controlador_doc_documento.php
@@ -5,6 +5,8 @@ use base\controller\controlador_base;
 use models\doc_documento;
 
 class controlador_doc_documento extends controlador_base{
+
+
     public function __construct($link){
         $modelo = new doc_documento($link);
         parent::__construct($link, $modelo);
