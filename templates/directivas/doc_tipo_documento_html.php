67236e386ec570fda84e718f66e11d9b8853901a
mgamboa <gamboa.martin@gmail.com>
2023-08-25 15:50:09 -0600

    v9.2.0 Se integra filter in rows t doc

--- templates/directivas/doc_tipo_documento_html.php
+++ templates/directivas/doc_tipo_documento_html.php
@@ -9,21 +9,25 @@ use PDO;
 class doc_tipo_documento_html extends html_controler {
 
     /**
+     * Genera un select con tipos de documento
      * @param int $cols
      * @param bool $con_registros
      * @param int $id_selected
      * @param PDO $link
      * @param bool $disabled
+     * @param array $filtro
      * @param array $registros
      * @return array|string
      */
     public function select_doc_tipo_documento_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
-                                                 bool $disabled = false, array $registros = array()): array|string
+                                                 bool $disabled = false, array $filtro = array(),
+                                                 array $registros = array()): array|string
     {
         $modelo = new doc_tipo_documento(link: $link);
 
         $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
-            modelo: $modelo, disabled: $disabled, label: 'Tipo Documento', registros: $registros, required: true);
+            modelo: $modelo, disabled: $disabled, filtro: $filtro, label: 'Tipo Documento', registros: $registros,
+            required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
